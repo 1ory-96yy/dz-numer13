be5cc66b3ad6395eda8a04504c12b9f6cdff7863
Artur <arturvasylkivsky@gmail.com>
2024-05-08 18:36:25 +0300

    dcfvbnm

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <initializer_list>
+
 template<typename T>
 T average(T val) {
     return val;
@@ -7,8 +8,18 @@ T average(T val) {
 
 template<typename T, typename... Args>
 T average(T first, Args... args) {
-    return (first + average(args...)) / (sizeof...(args) + 1);
+    return (first + average(args...));
 }
+
+template<typename T>
+T average(std::initializer_list<T> list) {
+    T sum = 0;
+    for (const auto& elem : list) {
+        sum += elem;
+    }
+    return sum / list.size();
+}
+
 template<typename T>
 class Array {
 private:
@@ -16,7 +27,10 @@ private:
     size_t size;
 public:
     Array(std::initializer_list<T> init_list) : size(init_list.size()), data(new T[size]) {
-        std::copy(init_list.begin(), init_list.end(), data);
+        size_t i = 0;
+        for (const T& elem : init_list) {
+            data[i++] = elem;
+        }
     }
 };
 
@@ -30,18 +44,10 @@ T minimum(T first, Args... args) {
     T min_of_rest = minimum(args...);
     return (first < min_of_rest) ? first : min_of_rest;
 }
-template<typename T>
-T average(std::initializer_list<T> list) {
-    T sum = 0;
-    for (const auto& elem : list) {
-        sum += elem;
-    }
-    return sum / list.size();
-}
 
 int main() {
-    std::cout << "Average: " << average(1, 2, 3, 4, 5) << std::endl; 
-    Array<int> arr = { 1, 2, 3, 4, 5 }; 
+    std::cout << "Average: " << average(1, 2, 3, 4, 5) / 5 << std::endl;
+    Array<int> arr = { 1, 2, 3, 4, 5 };
     std::cout << "Minimum: " << minimum(3, 1, 4, 1, 5, 9) << std::endl;
     std::cout << "Average: " << average({ 1, 2, 3, 4, 5 }) << std::endl;
 
